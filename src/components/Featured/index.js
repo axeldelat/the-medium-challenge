637ed9d4dfc0d6d168edd17e048c052c4d55e2ca
gommorin <luis.gomezmorin@gmail.com>
2020-11-12 16:01:07 -0600

    componentes migrados

---
+++ src/components/Featured/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+// CSS //
+import './Featured.css'
+
+class Featured extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  render() {
+
+  }
+}
